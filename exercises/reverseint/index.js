e34b305d1b8a8bb403de1eb097ae383bfa8a1894
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-08-08 23:23:35 -0700

    simple version of reverse integer is working

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,10 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    str = n.toString();
+    reverse = str.split('').reverse().join("");
+    return parseInt(reverse)
+}
 
 module.exports = reverseInt;
