29f1c33f00c71e9f63d0b74913234bedbc8927e8
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-08-09 22:49:08 -0700

    adding second version of maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,48 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// function maxChar(str) {
+//     const chars = {};
+
+
+//     for (i = 0; i < str.lenght; i++) {
+//         if (chars[char]) {
+//             chars[char]++
+//         } else {
+//             chars[char] = 1;
+//         }
+//     }
+// }
+
+// // for (let char of str) {
+// //     chars[char] = chars[char] + 1 || 1;
+// //    return chars
+// // }
+
+// // }
+
+//version #1
+
+const string = "Hello There!"
+const chars = {};
+
+for (let char of string) {
+    if ( !chars[char]) {
+        chars[char] = 1;
+    } else {
+        chars[char]++
+    }
+}
+
+console.log(chars)
+
+// version #2
+const strings = "Hello There!"
+const charss = {};
+
+for (cha of strings) {
+    charss[cha] = charss[cha] + 1 || 1;
+}
+console.log(charss)
 
 module.exports = maxChar;
