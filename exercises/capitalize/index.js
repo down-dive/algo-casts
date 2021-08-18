be9aff82a7f3da41af7ac9057e363b97ff3b9897
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-08-18 10:44:42 -0700

    making changes to capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,8 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
+
+
 // first version
 function capitalize(str) {
     const words = [];
